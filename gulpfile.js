a82bcf8f37f949f608dc4486ec2345d737b3aac5
Lucas Ramos <lucasrcdias@gmail.com>
2016-09-27 08:24:05 -0300

    Remove stylus e usa sass, melhora workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,21 @@
-var gulp        = require('gulp');
-var pug         = require('gulp-pug');
-var fastylus    = require('fa-stylus');
-var stylus      = require('gulp-stylus');
-var uglify      = require('gulp-uglify');
-var koutoSwiss  = require('kouto-swiss');
-var plumber     = require('gulp-plumber');
-var cssnano     = require('gulp-cssnano');
-var browserSync = require('browser-sync');
-var prefixer    = require('autoprefixer-stylus');
+var gulp            = require('gulp');
+var pug             = require('gulp-pug');
+var sass            = require("gulp-sass");
+var watch           = require("gulp-watch");
+var concat          = require("gulp-concat");
+var uglify          = require('gulp-uglify');
+var plumber         = require('gulp-plumber');
+var cssnano         = require('gulp-cssnano');
+var browserSync     = require('browser-sync');
+var autoprefixer    = require("gulp-autoprefixer");
+var angularFileSort = require("gulp-angular-filesort");
 
 var srcPaths = {
   js:       'src/js/**/*.js',
   pug:      'src/views/*.pug',
   pugIndex: 'src/index.pug',
-  css:      'src/stylus/**/*.styl',
-  mainStyl: 'src/stylus/main.styl'
+  css:      'src/style/**/*.sass',
+  mainSass: 'src/style/main.sass'
 };
 
 var buildPaths = {
@@ -24,22 +25,37 @@ var buildPaths = {
   pug:   'dist/views/'
 };
 
+var autoprefixerOptions = {
+  browsers: [
+      '> 1%',
+      'last 2 versions',
+      'firefox >= 4',
+      'safari 7',
+      'safari 8',
+      'IE 8',
+      'IE 9',
+      'IE 10',
+      'IE 11'
+  ],
+  cascade: false
+};
+
 gulp.task('css', function() {
-  gulp.src(srcPaths.mainStyl)
+  gulp.src(srcPaths.mainSass)
     .pipe(plumber())
-    .pipe(stylus({
-      use: [koutoSwiss(), prefixer(), fastylus()],
-      compress: true
-    }))
+    .pipe(sass())
+    .pipe(autoprefixer(autoprefixerOptions))
     .pipe(cssnano())
     .pipe(gulp.dest(buildPaths.css));
 });
 
-gulp.task('js', function() {
+gulp.task("js", function () {
   gulp.src(srcPaths.js)
     .pipe(plumber())
+    .pipe(angularFileSort())
+    .pipe(concat("viewpoint.min.js"))
     .pipe(uglify())
-    .pipe(gulp.dest(buildPaths.js));
+    .pipe(gulp.dest(buildPaths.build));
 });
 
 gulp.task('pug', function() {
@@ -57,10 +73,21 @@ gulp.task('pugIndex', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch(srcPaths.css,      ['css']);
-  gulp.watch(srcPaths.js,       ['js']);
-  gulp.watch(srcPaths.pug,      ['pug']);
-  gulp.watch(srcPaths.pugIndex, ['pugIndex']);
+  watch(srcPaths.js, function () {
+    gulp.start("js");
+  });
+
+  watch(srcPaths.css, function () {
+    gulp.start("css");
+  });
+
+  watch(srcPaths.pug, function () {
+    gulp.start("pug");
+  });
+
+  watch(srcPaths.pugIndex, function () {
+    gulp.start("pugIndex");
+  });
 });
 
 gulp.task('browser-sync', function() {
