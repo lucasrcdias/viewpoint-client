35edac4f848eb2881f3514690b4475f599c927ee
Lucas Ramos <lucasrcdias@gmail.com>
2016-10-03 08:37:33 -0300

    Move declaração das rotas para modulo separado

--- src/js/routes/viewpoint.routes.js
+++ src/js/routes/viewpoint.routes.js
@@ -1,18 +1,25 @@
 (function() {
   angular
-    .module('viewpoint.core')
+    .module('viewpoint.routes')
     .config(viewpointRoutes);
 
   viewpointRoutes.$inject = ["$stateProvider", "$urlRouterProvider"];
 
   function viewpointRoutes($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.otherwise("/home");
 
     $stateProvider
       .state("home", {
-        url: "/",
+        url: "/home",
         templateUrl: "/views/home.html",
         controller: "homeCtrl",
+        controllerAs: "vm",
+        bindToController: true
+      })
+      .state("signup", {
+        url: "/cadastre-se",
+        templateUrl: "/views/signup.html",
+        controller: "signupCtrl",
         controllerAs: "vm",
         bindToController: true
       });
