dcc307e61bdd0e23a781f9cfe5d834eb9a24c958
Lucas Ramos <lucasrcdias@gmail.com>
2016-10-23 02:34:51 -0200

    Cria serviço como mock para carregamento de grupos do usuário

---
+++ src/js/services/groups.service.js
@@ -0,0 +1,33 @@
+(function () {
+  angular
+    .module("viewpoint.services")
+    .factory("groupsService", groupsService);
+
+  function groupsService() {
+    var service = {
+      getGroups: getGroups
+    };
+
+    return service;
+
+    function getGroups() {
+      return [
+        {
+          id: 1,
+          name: "Página de busca"
+        },
+        {
+          id: 2,
+          name: "Página de compra"
+        },
+        {
+          id: 3,
+          name: "Página de cadastro"
+        },{
+          id: 4,
+          name: "Página incial"
+        }
+      ];
+    }
+  };
+})();
