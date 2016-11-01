7de0d1610cc7fd01401054f417547764056bc956
Lucas Ramos <lucasrcdias@gmail.com>
2016-11-01 08:24:41 -0200

    Implementa alteração de senha

--- src/js/controllers/password-update.controller.js
+++ src/js/controllers/password-update.controller.js
@@ -3,9 +3,42 @@
     .module("viewpoint.controllers")
     .controller("passwordUpdateCtrl", passwordUpdateCtrl);
 
-  passwordUpdateCtrl.$inject = ["userService"];
+  passwordUpdateCtrl.$inject = ["userService", "errorsService", "$state", "alertService"];
 
-  function passwordUpdateCtrl (userService) {
+  function passwordUpdateCtrl (userService, errorsService, $state, alertService) {
     var vm = this;
+
+    vm.user = {};
+
+    vm.fieldErrors        = fieldErrors;
+    vm.passwordFormSubmit = passwordFormSubmit;
+
+    function passwordFormSubmit (user) {
+      return userService.update(user)
+        .then(onSuccess)
+        .catch(onFailure)
+        .finally(clearPassword);
+    }
+
+    function onSuccess (response) {
+      alertService.success("Sua senha foi alterada com sucesso!");
+
+      $state.go("dashboard");
+    }
+
+    function onFailure (error) {
+      vm.user.errors = errorsService.parse(error.data);
+    }
+
+    function clearPassword () {
+      vm.user.password = "";
+      vm.user.passwordConfirmation = "";
+
+      vm.passwordForm.$setPristine();
+    }
+
+    function fieldErrors (field) {
+      return errorsService.normalize(vm.passwordForm, vm.user, field);
+    }
   }
 })();
