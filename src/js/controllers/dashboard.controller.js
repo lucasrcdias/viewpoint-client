89dcf23fa3c321b10bcfa67c1481aca97d20107a
Lucas Ramos <lucasrcdias@gmail.com>
2016-11-03 07:49:12 -0200

    Lista os grupos e realiza o filtro

--- src/js/controllers/dashboard.controller.js
+++ src/js/controllers/dashboard.controller.js
@@ -3,16 +3,27 @@
     .module("viewpoint.controllers")
     .controller("dashboardCtrl", dashboardCtrl);
 
-  dashboardCtrl.$inject = ["groups"];
+  dashboardCtrl.$inject = ["groupsService", "groups"];
 
-  function dashboardCtrl (groups) {
+  function dashboardCtrl (groupsService, groups) {
     var vm = this;
 
-    vm.groups = groups;
+    vm.groups = normalizeGroups(groups);
 
-    vm.groupSearch = groupSearch;
+    vm.groupSearch  = groupSearch;
+    vm.filterEvents = filterEvents;
+
+    function filterEvents (group) {
+      return groupsService.getEvents(group.originalObject)
+        .then(onSuccess)
+        .catch(onFailure);
+    }
 
     function groupSearch (query) {
+      if (!vm.groups.length) {
+        return [];
+      }
+
       return vm.groups.filter(function(group) {
         return normalizedName(group.name).search(query) !== -1;
       });
@@ -20,6 +31,25 @@
 
     function normalizedName (name) {
       return name.removeDiacritics().toLowerCase();
+    }
+
+    function onSuccess (response) {
+      console.log(response.data);
+    }
+
+    function onFailure (error) {
+      console.log(error);
+    }
+
+    function normalizeGroups (groups) {
+      var userGroups = groups.data.groups;
+      var normalizedGroups = [];
+
+      angular.forEach(userGroups, function (group) {
+        normalizedGroups.push({ "name": group });
+      });
+
+      return normalizedGroups;
     }
   }
 })();
