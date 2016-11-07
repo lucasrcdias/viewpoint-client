4bfc88a146b16b7668dbfd1926c4d92528c613b8
Lucas Ramos <lucasrcdias@gmail.com>
2016-11-07 10:19:18 -0200

    Melhora responsividade do gráfico

--- src/js/directives/vp-report.directive.js
+++ src/js/directives/vp-report.directive.js
@@ -18,6 +18,8 @@
     function linkFunction (scope, element, attributes) {
       if (!google) { return; }
 
+      var $el = element[0];
+
       google.charts.load('current', { 'packages': ['corechart'] });
       google.charts.setOnLoadCallback(drawChart);
 
@@ -28,16 +30,20 @@
       });
 
       function drawChart () {
+        console.log("Drawning...");
         if (!scope.data || !scope.data.length) { return; }
 
         var events  = parsedEvents(scope.data);
+        var width   = $el.offsetWidth;
         var data    = google.visualization.arrayToDataTable(events);
         var options = {
           "title": "Eventos do grupo " + scope.group,
-          "width": element[0].parentElement.offsetWidth
+          "width": width
         }
 
-        var chart = new google.visualization.PieChart(element[0]);
+        console.log(width);
+
+        var chart = new google.visualization.PieChart($el);
 
         chart.draw(data, options);
       }
